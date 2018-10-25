61b60679c73f38136a278ee1388c08f9c48473e2
lukalev <luka.levstek@gmail.com>
2018-10-25 02:34:58 +0200

    Updates

---
+++ precache-manifest.780cfa23f77e73e7f54d2e9bafcee54d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8911fa0be5564eaca1cf",
+    "url": "/crypto-list/static/js/runtime~main.8911fa0b.js"
+  },
+  {
+    "revision": "172cff8627a2223500a6",
+    "url": "/crypto-list/static/js/main.172cff86.chunk.js"
+  },
+  {
+    "revision": "6df7be01ea6b2c004196",
+    "url": "/crypto-list/static/js/1.6df7be01.chunk.js"
+  },
+  {
+    "revision": "172cff8627a2223500a6",
+    "url": "/crypto-list/static/css/main.53d93d3e.chunk.css"
+  },
+  {
+    "revision": "6df7be01ea6b2c004196",
+    "url": "/crypto-list/static/css/1.4f60ae52.chunk.css"
+  },
+  {
+    "revision": "ebcfeac92d7073f8d7e41f30677260ee",
+    "url": "/crypto-list/index.html"
+  }
+];
